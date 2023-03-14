75f8961ea5ccfc623fe7746af29ee1d7bd458a91
Haining Yu <1210460391@qq.com>
2023-03-15 05:08:48 +0800

    Adapt to Apple Mac OS

--- WordLab/Program/CsvQuery/all.cpp
+++
@@ -1,4 +0,0 @@
-#include "CSVQuery\\all.cpp"
-#include "input\\all.cpp"
-#include "version.cpp"
-#include "shell.cpp"
---
+++ WordLab/program/csv-query/all.cpp
@@ -0,0 +1,4 @@
+#include "CSVQuery/all.cpp"
+#include "input/all.cpp"
+#include "version.cpp"
+#include "shell.cpp"
