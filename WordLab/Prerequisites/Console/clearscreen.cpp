53c8f29221cf80af03dba2657f5fabfe54bfd491
user <user@company.com>
2023-04-30 10:20:23 +0800

    update

--- WordLab/Prerequisites/Console/clearscreen.cpp
+++ WordLab/Prerequisites/Console/clearscreen.cpp
@@ -1,6 +1,6 @@
 void clearscreen(const int x,const int y,const int width,const int height,const char fill){
-	for(int r=0;r<height;r++){
-	    gotoxy(x,y+r);
+	for(int r = 0; r < height; r++) {
+	    gotoxy(x, y + r);
 	    std::cout<<std::setw(width)<<std::setfill(fill)<<"";
 	}
 }
--- WordLab/program/Word/WordLinkage/Framework/Core.cpp
+++ WordLab/program/Word/WordLinkage/Framework/Core.cpp
@@ -1,6 +1,7 @@
 int WLFrameworkCore(string s) {
 	int n = 0, ReturnLetter = 0;
 	string s2(s);
+    clearscreen(0, 0, ScreenX - 1, 3);
 	WLHistory.RecordSearch(s,0);
 	if (BL) {
         n = WLSearch(s,0);//UPPER-LOWER CASE SENSITIVE
@@ -8,23 +9,23 @@ int WLFrameworkCore(string s) {
 	if (BL && n == 0) {
         n = WLSearch(s,1);//UPPER_LOWER CASE IGNORED
     }
-    printf("search result is %d\n", n);
+//    printf("search result is %d\n", n);
 	if (BL && n == 0) {
         n = WLSearchFuzzy(s.c_str(),1);
     }
 	if (DL) {
-        printf("\nprefix n = %d, s2= %s\n", n, s2.c_str());
+//        printf("\nprefix n = %d, s2= %s\n", n, s2.c_str());
         prefix(s2);
     }
 	if (SL) {
-        printf("\nWLSearchSimilarSpelling s = %s\n", s.c_str());
+//        printf("\nWLSearchSimilarSpelling s = %s\n", s.c_str());
         WLSearchSimilarSpelling(s,2);
     }
 	if (CL && n == 0) {
-        printf("\nAddNew s = %s\n", s.c_str());
+//        printf("\nAddNew s = %s\n", s.c_str());
 		AddNew(s);
 		ReturnLetter = 13;
 	}
-    printf("\nWLFrameworkCore returns %d\n", ReturnLetter);
+//    printf("\nWLFrameworkCore returns %d\n", ReturnLetter);
 	return ReturnLetter;
 }
