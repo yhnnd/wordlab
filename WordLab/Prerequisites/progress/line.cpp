7ad08221e0d0a8ed439ef1ea7c05ee7f3e375728
user <user@company.com>
2023-05-02 12:58:40 +0800

    update

--- WordLab/Prerequisites/progress/line.cpp
+++ WordLab/Prerequisites/progress/line.cpp
@@ -1,8 +1,10 @@
 void progressbar::line(int x,int y,int nprev,int n,char s,WORD color){
-	int r=nprev;
-	gotoxy(x+r,y);
+	int r = nprev;
+	gotoxy(x + r, y);
 	colorrecord(colorprev);
 	colorset(color);
-	for(;r<n;r++) std::cout<<s;
+	for (; r < n; r++) {
+        std::cout<<s;
+    }
 	colorreset(colorprev);
 }
