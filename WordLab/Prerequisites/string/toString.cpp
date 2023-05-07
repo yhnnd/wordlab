3ca9c1984d780443a03bfdf9af2640dedd6e58ef
user <user@company.com>
2023-05-07 14:24:12 +0800

    update

--- WordLab/Prerequisites/string/toString.cpp
+++ WordLab/Prerequisites/string/toString.cpp
@@ -1,8 +1,8 @@
 template <typename T>
-std::string toString(const T & object){
+std::string toString(const T & object) {
 	std::string s;
 	std::stringstream ss;
-	ss<<object;
-	ss>>s;
+	ss << object;
+	ss >> s;
 	return s;
 }
