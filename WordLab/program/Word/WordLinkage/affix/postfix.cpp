2b400111ae6011fbbae1e4ed1996406cfd955114
user <user@company.com>
2023-05-16 18:51:39 +0800

    update affix

--- WordLab/program/Word/WordLinkage/affix/postfix.cpp
+++ WordLab/program/Word/WordLinkage/affix/postfix.cpp
@@ -1,9 +1,9 @@
 int postfix(int lth,string word) {
 	std::vector<std::vector<std::string>> affix = {
 		{"s","#"},
-		{"en","ly","cy","ry","al","er","or","ic","fy","ss","th","#"},
-		{"ant","ary","age","ate","ble","ect","ent","est","ful","gue","ice","ise","ist","ive","ize","ion","ity","ism","ing","ous","ply","yze","yse","#"},
-		{"able","ible","tual","icit","ment","ness","less","some","tain","tion","ture","#"},
+		{"en","ly","cy","al","er","or","ic","fy","th","#"},
+		{"ant","ary","age","ate","ble","ect","ent","ess","est","ful","gue","ice","ise","ist","ive","ize","ion","ity","ism","ing","ory","ous","ply","ury","yze","yse","#"},
+		{"able","ible","tual","icit","ment","ness","less","some","ster","tain","tion","ture","#"},
 		{"cient","ation","istic","stant","spect","#"},
 		{"stance","#"},
 		{"#"},
