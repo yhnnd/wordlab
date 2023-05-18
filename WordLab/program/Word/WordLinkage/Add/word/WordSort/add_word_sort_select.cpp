de5fe41207fc31865e6f5e29da4d276ed9e11761
user <user@company.com>
2023-05-19 00:31:23 +0800

    u

--- WordLab/program/Word/WordLinkage/Add/word/WordSort/add_word_sort_select.cpp
+++ WordLab/program/Word/WordLinkage/Add/word/WordSort/add_word_sort_select.cpp
@@ -14,7 +14,9 @@ string AddWordSortSelect(const int x, const int y) {
 
 	MessageWindow.Frame(0, x, y, width, optionsMenu);
 	choice = MessageWindow.Switch(x, y, width, optionsMenu, choice);
-    MessageWindow.Frame(-1, x, y, width, optionsMenu.size(), 0);
+    if (choice != 5) {
+        MessageWindow.Frame(-1, x, y, width, optionsMenu.size(), 0);
+    }
 
 	MessageWindow.SetMonochrome(isMonochromePrev);
 
@@ -26,6 +28,7 @@ string AddWordSortSelect(const int x, const int y) {
 	    case 5:
             char s[16];
             listen(s, 0, 1, {13, 10, 0, EOF}, sizeof(s));
+            MessageWindow.Frame(-1, x, y, width, optionsMenu.size(), 0);
             return s;
             break;
 	    default:
