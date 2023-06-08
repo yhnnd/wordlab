dbcf610c1a2b882c41eb2ab89e4ed8a98a4be1fa
user <user@company.com>
2023-06-08 16:35:06 +0800

    up

--- WordLab/program/Word/WLChinese/WLChineseCore.cpp
+++ WordLab/program/Word/WLChinese/WLChineseCore.cpp
@@ -1,33 +1,51 @@
-string WLChineseCore(string line, const int fontcolor) {
+std::string WLChineseCore(const std::string line, const int fontcolor) {
+
+    const auto sanctifyChar = [] (const char ch) {
+        const std::string prefix = strchr("()<>", ch) ? "\\" : "";
+        return prefix + ch;
+    };
+
+    const auto sanctify = [&sanctifyChar] (const std::string text) {
+        std::string result = "";
+        for (int i = 0; i < text.length(); ++i) {
+            result += sanctifyChar(text[i]);
+        }
+        return result;
+    };
+
 	string result_line = "";
-	int ver = 0;
-	bool flagdef = false, flagsort = false;
-	for(int r = 0; r<line.length(); r++) {
+	int numOfDefs = 0;
+	bool isInsideDefinition = false, isInsideSort = false;
+	for(int r = 0; r < line.length(); r++) {
 		if (line[r] == ',') {
-            flagdef = true;
-        } else if (line[r]=='/') {
-			if ((flagsort=!flagsort)==true) {
+            isInsideDefinition = true;
+        } else if (isInsideDefinition && line[r]=='/') {
+			if ((isInsideSort = !isInsideSort) == true) {
                 result_line += "<blk-ylw>(";
             } else {
 				result_line += ")<" + getChineseColorCommandByNumber(fontcolor) + ">(";
 			}
-		} else if (line[r]=='@') {
-			result_line += line.substr(r,line.substr(r).find(";"));
-			r += line.substr(r).find(";")+1;
-			goto endDef;
-		} else if(line[r]==';') {
-endDef:
+		} else if (isInsideDefinition && line[r] == '@') {
+            const std::string::size_type textLength = line.substr(r).find(";");
+            if (textLength != std::string::npos) {
+                std::string definitionText = line.substr(r, textLength);
+                result_line += sanctify(definitionText);
+                r += textLength;
+                goto END_CURRENT_DEFINITION;
+            }
+		} else if (isInsideDefinition && line[r]==';') {
+END_CURRENT_DEFINITION:
 			result_line += ")";
-			flagdef=false;
-			ver++;
+            isInsideDefinition = false;
+			numOfDefs ++;
 		} else {
-            result_line += line[r];
+            result_line += sanctifyChar(line[r]);
         }
 	}
 	result_line += "<";
-	result_line += ver > 0 ? "blk-grn" : "blk-red";
+	result_line += numOfDefs > 0 ? "blk-grn" : "blk-red";
 	result_line += ">(v)<";
-	result_line += ver > 0 ? "blk-ylw" : "blk-red";
-	result_line += ">(" + toString(ver) + ")";
+	result_line += numOfDefs > 0 ? "blk-ylw" : "blk-red";
+	result_line += ">(" + toString(numOfDefs) + ")";
 	return result_line;
 }
