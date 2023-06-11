ccaa743371ee5fe0b87139e49c9a8814c9789260
user <user@company.com>
2023-06-12 00:41:05 +0800

    fix bugs

--- WordLab/Prerequisites/msgwin/MessageWindowLegacy/Confirm/Framework.cpp
+++ WordLab/Prerequisites/msgwin/MessageWindowLegacy/Confirm/Framework.cpp
@@ -1,11 +1,10 @@
 int MessageWindow::Confirm(const int n, const int x, const int y, const int windowWidth, const char *what, const int max, const int lineWidth) {
-    int r = 0;
     const int actualMax = MaxLength(what, max, lineWidth);
     recordxy(xy);
 	const bool background = SetBackground(1), monochrome = SetMonochrome(1);
     Frame(0, x, y, windowWidth, what, actualMax, lineWidth);
 //    printf("frame was printed\n");
-    r = ConfirmSwitch(n, x + 1, y + actualMax - 1, windowWidth + 5, 2);
+    const int r = ConfirmSwitch(n, x + 1, y + actualMax - 1, windowWidth + 5, 2);
     Frame(-1, x, y, windowWidth, actualMax, actualMax);
 	SetMonochrome(monochrome);
 	SetBackground(background);
@@ -14,12 +13,11 @@ int MessageWindow::Confirm(const int n, const int x, const int y, const int wind
 }
 
 int MessageWindow::Confirm(const int n, const int x, const int y, const int windowWidth, const std::vector<std::string> what) {
-    int choice = 0;
     const int actualMax = what.size();
     recordxy(xy);
     const bool background = SetBackground(1), monochrome = SetMonochrome(1);
     Frame(0, x, y, windowWidth, what);
-    choice = ConfirmSwitch(n, x + 1, y + actualMax - 1, windowWidth + 5, 2);
+    const int choice = ConfirmSwitch(n, x + 1, y + actualMax - 1, windowWidth + 5, 2);
     Frame(-1, x, y, windowWidth, actualMax, actualMax);
     SetMonochrome(monochrome);
     SetBackground(background);
--- WordLab/program/Word/WordLinkage/Add/Phrase/add_phrase_core.cpp
+++ WordLab/program/Word/WordLinkage/Add/Phrase/add_phrase_core.cpp
@@ -1,54 +1,103 @@
+std::string addPhraseInputNormalDefinitions() {
+    const vector<string> msgs = {"", "", "", "    continue appending definitions ?", "", "", "", "", ""};
+    const int x = 20, y = 8, width = 40;
+    string defs = "";
+    for (;;) {
+        defs = "";
+        for (;;) {
+            Dialog.output("input definition:");
+
+            {
+                std::string def = "";
+
+                ::fflush(stdin);
+                std::getline(cin, def);
+
+                defs += def;
+            }
+
+            const int choice = MessageWindow.Confirm(2, x, y, width, msgs);
+
+            if (choice != 1) {
+                break;// break inner loop
+            } else {
+                defs += ",";
+                Dialog.output("\"" + defs + "\"");
+            }
+        }
+        Dialog.output("use definitions \"" + defs + "\" ?");
+
+        ::fflush(stdin);
+        const char ch1 = getch();
+
+        if (ch1 == 13 || ch1 == 10) {
+            break;// break outer loop
+        } else {
+            Dialog.output("input definitions anew ?");
+            const char ch2 = getch();
+            if (ch2 == 13 || ch2 == 10) {
+                continue;
+            } else {
+                defs = "";
+                break;
+            }
+        }
+    }
+    return defs;
+}
+
+
+std::string addPhraseInputRedirectTarget () {
+    std::string defs = "";
+    for (;;) {
+        std::string target = "";
+        Dialog.output("input target:");
+
+        ::fflush(stdin);
+        std::getline(cin, target);
+
+        Dialog.output("use target \"" + target + "\" ?");
+
+        ::fflush(stdin);
+        const char ch3 = getch();
+
+        if (ch3 == 13 || ch3 == 10) {
+            defs = "/redirected./@target=\"" + target + "\"";
+            break;
+        } else {
+            Dialog.output("input target anew ?");
+            const char ch4 = getch();
+            if (ch4 == 13 || ch4 == 10) {
+                continue;
+            } else {
+                defs = "";
+                break;
+            }
+        }
+    }
+    return defs;
+}
+
+
 int addPhraseCore(const string msg, const enum AddMode mode) {
 
-	auto def = [&mode] {
+    const auto inputDefinitions = [&mode] () -> std::string {
         if (mode == AddMode::AddPhrase) {
-            const vector<string> msgs = {"", "", "", "    continue appending definitions ?", "", "", "", "", ""};
-            const int x = 20, y = 8, width = 40;
-            string defs = "", def = "";
-            for (;;) {
-                defs = "";
-                for (;;) {
-                    Dialog.output("input definition:");
-                    ::fflush(stdin);
-                    cin >> def;
-                    defs += def;
-                    const int choice = MessageWindow.Confirm(2, x, y, width, msgs);
-                    if (choice != 1) {
-                        break;// break inner loop
-                    } else {
-                        defs += ",";
-                        Dialog.output("\"" + defs + "\"");
-                    }
-                }
-                Dialog.output("use definitions \"" + defs + "\" ?");
-                ::fflush(stdin);
-                const char ch = getch();
-                if (ch == 13 || ch == 10) {
-                    break;// break outer loop
-                }
-            }
-            return defs;
+            return addPhraseInputNormalDefinitions();
         } else if (mode == AddMode::RedirectPhrase) {
-            std::string defs = "";
-            for (;;) {
-                std::string target = "";
-                Dialog.output("input target:");
-                std::getline(cin, target);
-                Dialog.output("use target \"" + target + "\" ?");
-                ::fflush(stdin);
-                const char ch = getch();
-                if (ch == 13 || ch == 10) {
-                    defs = "/redirected./@target=\"" + target + "\"";
-                    break;
-                }
-            }
-            return defs;
+            return addPhraseInputRedirectTarget();
         } else {
-            return std::string ("");
+            return "";
         }
-	};
+    };
 
-	string s = "," + msg + "," + def() + ";";
+    const std::string definitions = inputDefinitions();
+
+    if (definitions.empty()) {
+        return 0;
+    }
+
+	std::string s = "," + msg + "," + definitions + ";";
 
     int numOfWords = 1;
 
